d85adcc5141dff4d57013aff105d09ad47c8db30
Manuel Puentes Espinoza <nnymmol@hotmail.com>
2017-09-29 02:07:46 -0700

    Basic teacher management view

--- app/scripts/controllers/teacher/student-management.js
+++ app/scripts/controllers/teacher/student-management.js
@@ -8,10 +8,14 @@
  * Controller of the noBullApp
  */
 angular.module('noBullApp')
-  .controller('TeacherStudentManagementCtrl', function () {
+  .controller('TeacherStudentManagementCtrl', ['$scope',function ($scope) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
-  });
+    console.log("im student manager");
+    $scope.activeStudents = [{'name':'Manuel Puentes', 'email':'mpuentes@cetys.edu.mx','groups':['cc401','phy123'],'lastActivity':'12/23/17'},
+    {'name':'Allan CAstro', 'email':'acastro@cetys.edu.mx','groups':['cc401','phy123'],'lastActivity':'10/10/17'},
+    {'name':'Fidel Martistro', 'email':'fiddy@cetys.edu.mx','groups':['cc401','phy123'], 'lastActivity':'04/09/16'}]
+  }]);
