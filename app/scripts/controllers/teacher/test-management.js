d0735bb279a22e3742ba834580bb4c9f4513d83e
Unknown <fidel.castro@cetys.edu.mx>
2017-11-15 16:29:44 -0800

    Test-management PUT

--- app/scripts/controllers/teacher/test-management.js
+++ app/scripts/controllers/teacher/test-management.js
@@ -23,6 +23,8 @@ angular.module('noBullApp')
         $scope.activeTest = {
           "title": "", "confidence":70, "tries": 1, "teacher_id": $scope.teacher_id, "subject": "New Test", "questions": [{'text':'', 'weighting':10}]
         };
+        console.log("change active test to", $scope.activeTest);
+
       }
       for(var i=0; i<$scope.tests.length;i++){
         if($scope.tests[i].test_id==testId){
@@ -30,10 +32,10 @@ angular.module('noBullApp')
           $scope.activeTestQuestionCounter=1;
           console.log("change active test to", $scope.activeTest);
         }
-
       }
     }
     $scope.changeActiveTest('newTest');
+
 
     $scope.postTest = function() {
       var body = {
@@ -49,14 +51,52 @@ angular.module('noBullApp')
 
         console.log("Post response");
         console.log(dataResponse);
+        alert('Your test has been created');
         $route.reload();
       });
 
     }
 
+    $scope.putTest = function() {
+      var putBody = {
+        expression: "set title=:t, subject=:s, questions=:q",
+        attributes: {
+          ":t": $scope.activeTest.title,
+          ":s": $scope.activeTest.subject,
+          ":q": $scope.activeTest.questions //TODO question_id
+        }
+       }
+
+       var test_id = $scope.activeTest.test_id
+
+      console.log('BODY: ', putBody);
+      console.log('ATTR: ', putBody.attributes[':q']);
+      console.log('TEST ID: ',$scope.activeTest.test_id);
+      APIservice.putData('/tests?test_id='+test_id+'&teacher_id='+$scope.activeTest.teacher_id, putBody).then(function(dataResponse){
+
+        console.log("PUT response");
+        console.log(dataResponse);
+        alert('Your test has been updated');
+        $route.reload();
+      });
+    }
+
+    $scope.changeButton = function(){
+      console.log('ID: ', $scope.activeTest.test_id);
+      if($scope.activeTest.test_id == null){
+        console.log("Entre a la funcion del boton con POST");
+        $scope.postTest();
+      }
+      else{
+        console.log("Entre a la funcion del boton con PUT");
+        $scope.putTest();
+      }
+
+    }
+
     $scope.addQuestion = function(){
       $scope.activeTest.questions.push({'text':'', 'weighting':10});
-    }
+    };
 
 
 
